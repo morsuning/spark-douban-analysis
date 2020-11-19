08ef05bcad072d2d6da10dea8fca1b50459489ec
xuechenyang <morsuning@gmail.com>
2020-11-20 00:28:50 +0800

    test

--- src/main/java/edu/nju/SparkStreamingApp.java
+++ src/main/java/edu/nju/SparkStreamingApp.java
@@ -2,7 +2,6 @@ package edu.nju;
 
 import edu.nju.config.SparkConfig;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
@@ -22,19 +21,11 @@ public class SparkStreamingApp {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName(SparkConfig.APP_NAME).setMaster(SparkConfig.MASTER);
 
-        try (JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));){
+        try (JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1))){
 
             JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
 
-            lines.map(new Function<String, Object>() {
-                @Override
-                public Object call(String s) throws Exception {
-                    return null;
-                }
-            });
-
             JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
-
 
             JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
             JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey(Integer::sum);
@@ -43,8 +34,6 @@ public class SparkStreamingApp {
 
             jssc.start();
             jssc.awaitTermination();
-
-
 
         }catch (Exception e) {
             e.printStackTrace();
