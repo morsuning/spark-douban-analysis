acc1b818240c3b106bd91f73e96d36561ac71038
xuechenyang <morsuning@gmail.com>
2020-11-21 21:58:39 +0800

    demo

--- src/main/java/edu/nju/SparkStreamingApp.java
+++ src/main/java/edu/nju/SparkStreamingApp.java
@@ -1,19 +1,20 @@
 package edu.nju;
 
 import edu.nju.config.SparkConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
-import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import scala.Tuple2;
+import org.apache.spark.streaming.api.java.*;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
+import scala.Tuple2;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * @author xuechenyang(morsuning @ gmail.com)
@@ -21,18 +22,31 @@ import java.util.Arrays;
  */
 public class SparkStreamingApp {
 
+    private static final Pattern SPACE = Pattern.compile(" ");
 
-    public static void main(String[] args) {
-        JavaSparkContext sc = new JavaSparkContext(SparkConfig.MASTER,SparkConfig.APP_NAME);
-        sc.setLogLevel("WARN");
+    String brokers = "node1:9092";
+    String groupId = "0";
+    String topics = "topic1";
 
+    public void sparkListening(JavaSparkContext sc) {
         try (JavaStreamingContext jssc = new JavaStreamingContext(sc, Durations.seconds(1))){
-            JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
+            Set<String> topicsSet = new HashSet<>(Arrays.asList(topics.split(",")));
+            Map<String, Object> kafkaParams = new HashMap<>();
+            kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+            kafkaParams.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+            kafkaParams.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+            kafkaParams.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
-            JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
+            JavaInputDStream<ConsumerRecord<String, String>> messages = KafkaUtils.createDirectStream(jssc,
+                    LocationStrategies.PreferConsistent(),
+                    ConsumerStrategies.Subscribe(topicsSet, kafkaParams));
 
-            JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
-            JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey(Integer::sum);
+             JavaDStream<String> lines = messages.map(ConsumerRecord::value);
+
+             lines.print();
+             JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(SPACE.split(x)).iterator());
+            JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1))
+                    .reduceByKey(Integer::sum);
 
             wordCounts.print();
 
@@ -42,5 +56,19 @@ public class SparkStreamingApp {
         }catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void compute(JavaDStream<String> lines) {
+
+    }
+
+
+
+    public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf().setAppName(SparkConfig.APP_NAME);
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+        sc.setLogLevel("WARN");
+        SparkStreamingApp sparkStreamingApp = new SparkStreamingApp();
+        sparkStreamingApp.sparkListening(sc);
     }
 }
--- src/main/java/edu/nju/config/SparkConfig.java
+++ src/main/java/edu/nju/config/SparkConfig.java
@@ -6,7 +6,7 @@ package edu.nju.config;
  */
 public class SparkConfig {
 
-    public static final String APP_NAME = "douban-analysis";
+    public static final String APP_NAME = "spark-douban-analysis";
 
     public static final String MASTER = "spark://namenode:7077";
 }
